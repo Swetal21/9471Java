ed27b50f079a042e96906eeb423a27207035ed3c
Swetal Ninave <95091493+Swetal21@users.noreply.github.com>
2021-12-03 20:11:46 +0530

    Add files via upload

---
+++ Anudip Practicals/Icebreaking/Compare.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Compare {
+ public static void main( String args[] )    
+    {
+               
+        Scanner input = new Scanner(System.in);
+        int number1;      
+        int number2; 
+      
+        System.out.print( "Input first integer: " ); 
+        number1 = input.nextInt();
+        System.out.print( "Input second integer: " );   
+        number2 = input.nextInt();            
+        
+        if ( number1 == number2 )           
+            System.out.printf( "%d == %d\n", number1, number2 );  
+        if ( number1 != number2 )          
+            System.out.printf( "%d != %d\n", number1, number2 );  
+        if ( number1 < number2 )          
+            System.out.printf( "%d < %d\n", number1, number2 );  
+        if ( number1 > number2 )          
+            System.out.printf( "%d > %d\n", number1, number2 );  
+        if ( number1 <= number2 )          
+            System.out.printf( "%d <= %d\n", number1, number2 );  
+        if ( number1 >= number2 )          
+            System.out.printf( "%d >= %d\n", number1, number2 );  
+    }
+  }
