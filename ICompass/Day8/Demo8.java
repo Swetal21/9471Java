4be5dd053902c1f7e76810ba9a57d39b3ba282f2
Swetal Ninave <95091493+Swetal21@users.noreply.github.com>
2022-01-11 16:46:08 +0530

    Add files via upload

---
+++ ICompass/Day8/Demo8.java
@@ -0,0 +1,14 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+class Demo8 {
+public static void main(String[]args)
+{
+	Path javaHome = Paths.get("C:/Program Files/Java/jdk-17.0.1");
+			System.out.println(javaHome.getNameCount());
+
+
+}
+}
+
+.
