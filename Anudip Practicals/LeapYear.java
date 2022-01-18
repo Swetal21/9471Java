8b1a8b5dd9eca144325598f02c8318c9afe1d0c0
Swetal Ninave <95091493+Swetal21@users.noreply.github.com>
2022-01-18 11:06:27 +0530

    Add files via upload

---
+++ Anudip Practicals/LeapYear.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class LeapYear {
+   public static void main(String[] args){
+      int year;
+      System.out.println("Enter an Year :: ");
+      Scanner sc = new Scanner(System.in);
+      year = sc.nextInt();
+
+      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+         System.out.println("It is a leap year");
+      else
+         System.out.println("It is not a leap year");
+   }
+}
