2ac65863de49f274711b265a13fbdeb09225da24
Swetal Ninave <95091493+Swetal21@users.noreply.github.com>
2022-01-29 15:13:11 +0530

    Add files via upload

---
+++ JPA_Sprint1/jpasampleinsert/src/main/java/Table.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Table {
+public static void main(String args[]) {
+	int num,i;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter number");
+   num=sc.nextInt();
+    i = 1;
+    num * i;
+   while(i<=10)
+   {
+	   
+	   
+	   System.out.println(num + " * " + i + " = "+ num * i);
+	   i++;     
+   
+   }
+	
+}
+	
+}
---
+++ JPA_Sprint1/jpasampleinsert/src/main/java/jpasampleinsert/jpasampleinsert/PersistStudent.java
@@ -0,0 +1,44 @@
+package jpasampleinsert.jpasampleinsert;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import jpasampleinsert.jpasampleinsert.entity.StudentEntity;
+
+public class PersistStudent 
+{
+    public static void main( String[] args )
+    {
+    	 EntityManagerFactory emf=Persistence.createEntityManagerFactory("Student_details");  
+         EntityManager em=emf.createEntityManager();  
+           
+ em.getTransaction().begin();  
+           
+         StudentEntity s1=new StudentEntity();  
+         s1.setS_id(101);  
+         s1.setS_name("Gaurav");  
+         s1.setS_age(24);  
+           
+         StudentEntity s2=new StudentEntity();  
+         s2.setS_id(102);  
+         s2.setS_name("Ronit");  
+         s2.setS_age(22);  
+           
+         StudentEntity s3=new StudentEntity();  
+         s3.setS_id(103);  
+         s3.setS_name("Rahul");  
+         s3.setS_age(26);  
+           
+         em.persist(s1);  
+         em.persist(s2);  
+         em.persist(s3);       
+   
+ em.getTransaction().commit();  
+           
+         emf.close();  
+         em.close();  
+           
+     }  
+    }
+
