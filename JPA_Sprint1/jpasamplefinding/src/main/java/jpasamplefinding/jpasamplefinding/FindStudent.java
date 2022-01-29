715f3e832be2b1159ab5c3f1572dc895de5b717a
Swetal Ninave <95091493+Swetal21@users.noreply.github.com>
2022-01-29 20:06:13 +0530

    Add files via upload

---
+++ JPA_Sprint1/jpasamplefinding/src/main/java/jpasamplefinding/jpasamplefinding/FindStudent.java
@@ -0,0 +1,33 @@
+package jpasamplefinding.jpasamplefinding;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import jpasamplefinding.jpasamplefinding.entity.StudentEntity;
+
+public class FindStudent 
+{
+    public static void main( String[] args )
+    {
+    	
+    	        EntityManagerFactory emf=Persistence.createEntityManagerFactory("Student_details");  
+    	        EntityManager em=emf.createEntityManager();  
+    	           
+    	       em.getTransaction().begin();
+    	       
+    	      
+    	          
+    	        StudentEntity s=em.find(StudentEntity.class,102);  
+    	          
+    	        System.out.println("Student id = "+s.getS_id());  
+    	        System.out.println("Student Name = "+s.getS_name());  
+    	        System.out.println("Student Age = "+s.getS_age());  
+    	       
+    	        em.getTransaction().commit();
+    	        emf.close();
+    	        em.close();
+    	    }  
+    	}
+    
+
